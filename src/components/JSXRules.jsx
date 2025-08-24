221f1af18daaa3a4e0d14bf26765469e0192fbff
MathiasNielsen99 <150657702+MathiasNielsen99@users.noreply.github.com>
2025-08-24 13:13:49 +0200

    refactor: remove unused files and simplify App component structure

---
+++ src/components/JSXRules.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const JSXRules = () => {
+  return (
+    <div>
+      <h1>JSXRules</h1>
+      <p>JSX must return a single root element.</p>
+      <p>JSX elements must be properly closed</p>
+      <p>JSX uses camelCase for attribute names eg className insted of class</p>
+    </div>
+  );
+};
+
+export default JSXRules;
